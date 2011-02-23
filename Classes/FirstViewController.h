650c056637f14a0cf3a31caf1dbc3a0f210f6ce6
Tak Tran <ttt@pebbleit.com>
2011-02-23 10:54:54 +0000

    Initial import of tab project.

---
+++ Classes/FirstViewController.h
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.h
+//  GreenLeague
+//
+//  Created by Tak Tran on 23/02/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FirstViewController : UIViewController {
+
+}
+
+@end
---
+++ Classes/GreenLeagueAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  GreenLeagueAppDelegate.h
+//  GreenLeague
+//
+//  Created by Tak Tran on 23/02/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GreenLeagueAppDelegate : NSObject <UIApplicationDelegate, UITabBarControllerDelegate> {
+    UIWindow *window;
+    UITabBarController *tabBarController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
+
+@end
