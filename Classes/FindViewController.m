0bc458aeaa87a84450b15193ca823f1f7003d94c
Tak <ttt@tbook.local>
2011-02-24 08:45:34 +0000

    Got detailed text label showing.

--- Classes/FindViewController.m
+++ Classes/FindViewController.m
@@ -107,7 +107,7 @@
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     if (cell == nil) {
-        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier] autorelease];
     }
     
 	int glDataIndex = [indexPath indexAtPosition:[indexPath length] - 1];
