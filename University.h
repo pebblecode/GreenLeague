2c8769ca5fe2c219ce4d7b478c4cc498884179a4
Tak Tran <ttt@pebbleit.com>
2011-02-25 10:37:16 +0000

    Adding University model.

---
+++ University.h
@@ -0,0 +1,56 @@
+//
+//  University.h
+//  GreenLeague
+//
+//  Created by Tak Tran on 25/02/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+
+@interface University :  NSManagedObject  
+{
+}
+
+@property (nonatomic, retain) NSNumber * performance11_1Data;
+@property (nonatomic, retain) NSNumber * performance8_2Score;
+@property (nonatomic, retain) NSString * rank2010;
+@property (nonatomic, retain) NSNumber * performance8_1Score;
+@property (nonatomic, retain) NSNumber * performance10_2Data;
+@property (nonatomic, retain) NSNumber * performance11_2Data;
+@property (nonatomic, retain) NSNumber * performanceScore;
+@property (nonatomic, retain) NSNumber * performance11Score;
+@property (nonatomic, retain) NSNumber * performance9_1Data;
+@property (nonatomic, retain) NSString * awardClass;
+@property (nonatomic, retain) NSNumber * performance9_2Score;
+@property (nonatomic, retain) NSNumber * policyScore;
+@property (nonatomic, retain) NSNumber * performance9_1Score;
+@property (nonatomic, retain) NSNumber * performance11_2Score;
+@property (nonatomic, retain) NSNumber * performance9_2Data;
+@property (nonatomic, retain) NSString * rank2009;
+@property (nonatomic, retain) NSNumber * performance11_1Score;
+@property (nonatomic, retain) NSNumber * performance10_2Score;
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSNumber * performance10_1Score;
+@property (nonatomic, retain) NSNumber * totalScore;
+@property (nonatomic, retain) NSNumber * performance9Score;
+@property (nonatomic, retain) NSNumber * performance8Score;
+@property (nonatomic, retain) NSNumber * policy1Score;
+@property (nonatomic, retain) NSNumber * performance8_1Data;
+@property (nonatomic, retain) NSNumber * policy2Score;
+@property (nonatomic, retain) NSNumber * policy3Score;
+@property (nonatomic, retain) NSNumber * performance8_2Data;
+@property (nonatomic, retain) NSNumber * policy4Score;
+@property (nonatomic, retain) NSNumber * policy5Score;
+@property (nonatomic, retain) NSNumber * performance10Score;
+@property (nonatomic, retain) NSNumber * performance8_3Data;
+@property (nonatomic, retain) NSNumber * policy6Score;
+@property (nonatomic, retain) NSNumber * policy7Score;
+@property (nonatomic, retain) NSNumber * performance10_1Data;
+@property (nonatomic, retain) NSNumber * performance8_3Score;
+
+@end
+
+
+
---
+++ University.m
@@ -0,0 +1,51 @@
+// 
+//  University.m
+//  GreenLeague
+//
+//  Created by Tak Tran on 25/02/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "University.h"
+
+
+@implementation University 
+
+@dynamic performance11_1Data;
+@dynamic performance8_2Score;
+@dynamic rank2010;
+@dynamic performance8_1Score;
+@dynamic performance10_2Data;
+@dynamic performance11_2Data;
+@dynamic performanceScore;
+@dynamic performance11Score;
+@dynamic performance9_1Data;
+@dynamic awardClass;
+@dynamic performance9_2Score;
+@dynamic policyScore;
+@dynamic performance9_1Score;
+@dynamic performance11_2Score;
+@dynamic performance9_2Data;
+@dynamic rank2009;
+@dynamic performance11_1Score;
+@dynamic performance10_2Score;
+@dynamic name;
+@dynamic performance10_1Score;
+@dynamic totalScore;
+@dynamic performance9Score;
+@dynamic performance8Score;
+@dynamic policy1Score;
+@dynamic performance8_1Data;
+@dynamic policy2Score;
+@dynamic policy3Score;
+@dynamic performance8_2Data;
+@dynamic policy4Score;
+@dynamic policy5Score;
+@dynamic performance10Score;
+@dynamic performance8_3Data;
+@dynamic policy6Score;
+@dynamic policy7Score;
+@dynamic performance10_1Data;
+@dynamic performance8_3Score;
+
+@end
