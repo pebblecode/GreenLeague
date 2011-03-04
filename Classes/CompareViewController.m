bf2e4f80820c56c94dc16ef11ee43029244f92b0
Tak Tran <ttt@pebbleit.com>
2011-03-04 17:49:13 +0000

    Adding compare view as table and compare button.

--- Classes/CompareViewController.m
+++ Classes/CompareViewController.m
@@ -12,18 +12,30 @@
 @implementation CompareViewController
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-	if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
+	if ((self = [super initWithNibName:@"CompareViewController" bundle:nibBundleOrNil])) {
 		self.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Compare" image:[UIImage imageNamed:@"12-eye.png"] tag:1];		
 	}
 	return self;
-}
+} 
 
-/*
+
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
+	
+	// Add button to the bottom of the table
+	UIView *compareButtonView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 50)];
+	UIButton *compareButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+	compareButton.frame = CGRectMake(60, 10, 200, 40);
+	[compareButton setTitle:@"Compare" forState:UIControlStateNormal];
+	
+	[compareButtonView addSubview:compareButton];		
+	self.tableView.tableFooterView = compareButtonView;
+
+	[compareButtonView release];
+	
 }
-*/
+
 
 /*
 // Override to allow orientations other than the default portrait orientation.
@@ -49,6 +61,53 @@
 
 - (void)dealloc {
     [super dealloc];
+}
+
+#pragma mark -
+#pragma mark Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+	return @"Compare:";
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    int numRows = 2;
+	
+    return numRows;
+}
+
+
+// Customize the appearance of table view cells.
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+    }
+    
+	cell.textLabel.text = @"hello";
+	
+    return cell;
+}
+
+#pragma mark -
+#pragma mark Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+	 <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+	 // ...
+	 // Pass the selected object to the new view controller.
+	 [self.navigationController pushViewController:detailViewController animated:YES];
+	 [detailViewController release];
+	 */
 }
 
 
