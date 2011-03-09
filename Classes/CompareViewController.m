ca64e657edfed3d14ef00930a55b209e1358ef95
Tak Tran <ttt@pebbleit.com>
2011-03-09 10:27:53 +0000

    Fixed up delete bug - needed to remove from the array first, then delete table (so that tableView:numberOfRowsInSection: returns the right value)

--- Classes/CompareViewController.m
+++ Classes/CompareViewController.m
@@ -165,11 +165,14 @@
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
 		
 	if (editingStyle == UITableViewCellEditingStyleDelete) {
-		// Delete the row from the data source.
-		[tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
-		
+        
+        // Remove from array
 		int rowToRemove = [indexPath row];
-		[self.universitiesToCompare removeObjectAtIndex:rowToRemove];
+		[self.universitiesToCompare removeObjectAtIndex:rowToRemove];        
+        
+		// Delete the row from table
+		[tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];		
+        
 	} else if (editingStyle == UITableViewCellEditingStyleInsert) {
 		// Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
 	}   
